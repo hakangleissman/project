85fc22bd60738d0af7593a179603443068cf4789
hakangleissman <hakan.gleissman@gmail.com>
2024-05-06 15:27:56 +0200

    a test

--- src/test/java/se/reky/hakan/TestScanner.java
+++ src/test/java/se/reky/hakan/TestScanner.java
@@ -1,4 +1,14 @@
 package se.reky.hakan;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
 public class TestScanner {
+    @Test
+    public void testScanner(){
+        Scanner scanner = new Scanner("test");
+        Assertions.assertEquals("test", scanner.nextLine());
+    }
 }
